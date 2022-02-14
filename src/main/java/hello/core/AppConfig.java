576a3d3131c99399aa1546f05df2e0807a67b020
changhyuk <changchang743@gmail.com>
2022-02-14 21:33:05 +0900

    test: bean 조회 확인

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -16,17 +16,20 @@ import hello.core.order.OrderServiceImpl;
 @Configuration
 public class AppConfig {
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
+
     @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+
     @Bean
-    public OrderService orderService(){
+    public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
     @Bean
     public DiscountPolicy discountPolicy() {
         // return new FixDiscountPolicy();
