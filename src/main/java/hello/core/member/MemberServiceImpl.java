0d862f944127d67170041c5b746f94210e8b0dc4
changhyuk <changchang743@gmail.com>
2022-02-16 22:07:31 +0900

    test: 의존관계 자동 주입 확인

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,24 +1,26 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository;
+	private final MemberRepository memberRepository;
 
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+	public MemberRepository getMemberRepository() {
+		return memberRepository;
+	}
 
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
+	@Override
+	public void join(Member member) {
+		memberRepository.save(member);
+	}
 
-    @Override
-    public void join(Member member) {
-        memberRepository.save(member);
-    }
-
-    @Override
-    public Member findMember(Long memberId) {
-        return memberRepository.findById(memberId);
-    }
+	@Override
+	public Member findMember(Long memberId) {
+		return memberRepository.findById(memberId);
+	}
 }
