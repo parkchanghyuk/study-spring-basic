dd573fc6c1bfe9a8127f20094eec5df6985351f1
changhyuk <changchang743@gmail.com>
2022-02-16 23:45:13 +0900

    test: spring life cycle 확인 (Bean init, destroy)

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 	public String url;
 
 	public NetworkClient() {
@@ -25,19 +22,17 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 	}
 
 	// 서비스 호출 후
-	public void disconnect(){
+	public void disconnect() {
 		System.out.println("close: " + url);
 	}
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
+	public void init() {
 		System.out.println("NetworkClient.afterPropertiesSet");
 		connect();
 		call("초기화 연결 메시지");
 	}
 
-	@Override
-	public void destroy() throws Exception {
+	public void close() {
 		System.out.println("NetworkClient.destroy");
 		disconnect();
 	}
