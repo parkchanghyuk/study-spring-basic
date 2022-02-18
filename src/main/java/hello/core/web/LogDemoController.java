75f9214215fe0f0c8cb13659bb316f20e0f0aa23
changhyuk <changchang743@gmail.com>
2022-02-18 17:13:25 +0900

    test: 스코프와  provider 확인

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,15 +15,17 @@ import lombok.RequiredArgsConstructor;
 public class LogDemoController {
 
 	private final LogDemoService logDemoService;
-	private final MyLogger myLogger;
+	private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
 	@RequestMapping("log-demo")
 	@ResponseBody
 	public String logDemo(HttpServletRequest request) {
 		String requestURL = request.getRequestURL().toString();
-		myLogger.setRequestURL(requestURL);
+		MyLogger myLogger = myLoggerObjectProvider.getObject();
 
+		myLogger.setRequestURL(requestURL);
 		myLogger.log("controller test");
+
 		logDemoService.logic("testId");
 		return "OK";
 	}
